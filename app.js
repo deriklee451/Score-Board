1176985ce61f74cc33e25e7a676f65a9b7c8627a
derik banning <deriklee451@gmail.com>
2022-10-31 15:44:12 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,54 @@
+let score = ''
+let scoreb = ''
+
+
+// home
+function increaseScore() {
+    score++
+    console.log(score)
+    draw()
+
+}
+
+
+function decreaseScore() {
+    score--
+    console.log(score)
+    draw()
+}
+
+// away
+function increaseScoreA() {
+    scoreb++
+    console.log(score)
+    drawA()
+
+}
+
+
+function decreaseScoreA() {
+    scoreb--
+    console.log(score)
+    drawA()
+}
+
+
+
+// Home Draw
+function draw() {
+
+    let points = document.getElementById("homeScore")
+
+    points.innerText = score
+
+}
+
+// Away Draw
+
+function drawA() {
+
+    let points = document.getElementById("away-score")
+
+    points.innerText = scoreb
+
+}
